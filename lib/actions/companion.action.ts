a6263d9a5322b1b0248475dfc15e707a00d7b64d
taha <rabia.ahmedtaha@univ-ouargla.dz>
2025-06-22 17:00:43 +0100

    implement getAllcComanion

--- lib/actions/companion.action.ts
+++ lib/actions/companion.action.ts
@@ -17,3 +17,26 @@ export const createCompanion = async (formData: CreateCompanion) => {
 
   return data[0];
 };
+
+export const getAllCompanion = async ({
+  limit = 10,
+  page = 1,
+  subject,
+  topic,
+}: GetAllCompanions) => {
+  const supabase = createSupabaseClient(); // connect oto DB
+  let query = supabase.from("Companions").select();
+  if (subject && topic) {
+    query = query
+      .ilike("name", `%${subject}%`)
+      .or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`);
+  } else if (subject) {
+    query = query.ilike(`subject`, `%${subject}%`);
+  } else if (topic) {
+    query = query.ilike(`topic.ilike.%${topic}%`, `name.ilike.%${topic}%`);
+  }
+  query = query.range((page - 1) * limit, page * limit - 1);
+  const { data: companions, error } = await query;
+  if (error) throw new Error(error.message);
+  return companions;
+};
